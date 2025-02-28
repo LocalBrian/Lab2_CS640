6b7ca0243200f0a7eec26cccda59ec0883521302
LocalBrian <150453691+LocalBrian@users.noreply.github.com>
2025-02-28 21:43:08 +0000

    Sudocode for verifyChecksum and handlePacket

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -85,7 +85,50 @@ public class Router extends Device
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
 		
+		// Check if IPv4 packet
+
+		// Subroutine to verify checksum
+
+		// Decrement TTL by 1
+
+		// Check if TTL is 0
+
+		// Check if destination IP is one of the router's interfaces
+
+		// Check if destination IP is in routing table
+
+		// Check ARP cache for MAC address
+
+		// Call sendPacket() with the appropriate arguments
 		
 		/********************************************************************/
+	}
+
+	/**
+	 * Handle an Ethernet packet received on a specific interface.
+	 * @param etherPacket the Ethernet packet that was received
+	 * @return Boolean true if checksum is valid, false otherwise
+	 */
+	public void verifyChecksum(Ethernet etherPacket)
+	{
+		
+		// Call ethernet.getPayload() to get the IP packet
+
+		// Cast result to packet of type IPv4
+
+		// Determine header length
+
+		// Get the checksum from the IP header
+
+		// Set the checksum in the IP header to 0
+
+		// Serialize the IP header with IPv4.serialize()
+
+		// Compute the checksum of the IP header
+
+		// Compare the computed checksum with the checksum from the IP header
+
+		// Return true if the checksums match, false otherwise
+		
 	}
 }
